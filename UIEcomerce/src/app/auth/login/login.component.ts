86e1ebb912aeecf4d176edec4cb59d378ef2904c
Pradel <prade516@gmail.com>
2025-09-24 17:35:45 -0300

    login no terminado

--- UIEcomerce/src/app/auth/login/login.component.ts
+++ UIEcomerce/src/app/auth/login/login.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { TaskService } from 'shared/services/task.service';
 
 @Component({
   selector: 'app-login',
@@ -12,5 +14,35 @@ import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
 export default class LoginComponent {
 
   authForm!: FormGroup;
+  constructor(
+    private readonly taskService: TaskService,
+    private readonly router: Router,
+    private readonly fb:FormBuilder
+  ){
+      this.Initial();
+  }
 
+  Initial():void{
+    this.authForm = this.fb.group({
+      UserName: ['',[Validators.required]],
+      UserPass: ['',[Validators.required]]
+    });
+  }
+  Submit(){
+    if( this.authForm.valid){
+      this.taskService.Authentication(this.authForm.value).subscribe(
+        (data:any) =>{
+          if(data){
+              this.router.navigate(['/principal']);
+          }
+          else{
+            alert('Usuario y/o contraseña incorrecto')
+          }
+        }
+      )
+    }
+    else{
+      alert('Debe ingresar los datos');
+    }
+  }
 }
