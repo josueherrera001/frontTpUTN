9ed74b0103c6e583ddf5a3c1aa58be8442a4e307
Pradel <prade516@gmail.com>
2025-10-15 00:21:18 -0300

    empezando con presentation

---
+++ UIEcomerce/src/app/modules/presentations/presentations.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-presentations',
+  standalone: true,
+  imports: [],
+  templateUrl: './presentations.component.html',
+  styleUrl: './presentations.component.scss'
+})
+export class PresentationsComponent {
+
+}
